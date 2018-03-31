9f5849aaa34d75fb824be5990b41e7b0f816970f
Laura Lynn <laura@iys.io>
2018-03-31 21:37:35 +0200

    pretty rough way of doing #1

--- lib/channel.ts
+++ lib/channel.ts
@@ -17,13 +17,45 @@ export class Channel {
 
 	async getCommandSettings(trigger: string, defaults: object | null): Promise<object | null> {
 		var settings = await this.db.hgetallAsync('channel:' + this.type + ':' + this.id + ':command:' + trigger);
+
 		if(!settings) {
 			settings = {};
 		}
+
 		if(defaults) {
 			for(let setting in defaults) {
 				if(defaults[setting].default != undefined && settings[setting] == undefined) {
 					settings[setting] = defaults[setting].default;
+				}
+
+				switch(defaults[setting].type) {
+					case 'boolean':
+						if(typeof settings[setting] == typeof true) break;
+
+						if(typeof settings[setting] == 'string') {
+							settings[setting] = settings[setting].toLowerCase().trim() == 'true';
+							break;
+						}
+
+						if(settings[setting]) {
+							settings[setting] = true;
+							break;
+						}
+
+						settings[setting] = defaults[setting].default;
+						break;
+
+					case 'number':
+						if(!isNaN(settings[setting]) && isFinite(settings[setting])) break;
+						settings[setting] = parseInt(settings[setting]);
+						if(!isNaN(settings[setting]) && isFinite(settings[setting])) break;
+						settings[setting] = defaults[setting].default;
+						break;
+
+					case 'string':
+						if(typeof settings[setting] == 'string') break;
+						settings[setting] = String(settings[setting]);
+						break;					
 				}
 			}
 		}
